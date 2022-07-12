8cc63788c127ea43a97cfb8f9968be606753ee9a
Minecraftschurli <minecraftschurli@gmail.com>
2022-07-12 18:05:52 +0200

    update forge version

--- src/main/java/com/github/ichhabehunger54/potionbundles/ClientHandler.java
+++ src/main/java/com/github/ichhabehunger54/potionbundles/ClientHandler.java
@@ -4,7 +4,7 @@ import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 //import net.minecraftforge.fml.ModList;
 //import net.minecraftforge.fml.ModLoadingContext;
@@ -23,9 +23,9 @@ public class ClientHandler {
     }
 
     @SubscribeEvent
-    public static void registerItemColorHandler(ColorHandlerEvent.Item e) {
-        e.getItemColors().register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.POTION_BUNDLE.get());
-        e.getItemColors().register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.SPLASH_POTION_BUNDLE.get());
-        e.getItemColors().register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.LINGERING_POTION_BUNDLE.get());
+    public static void registerItemColorHandler(RegisterColorHandlersEvent.Item e) {
+        e.register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.POTION_BUNDLE.get());
+        e.register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.SPLASH_POTION_BUNDLE.get());
+        e.register((stack, index) -> index > 0 ? -1 : PotionUtils.getColor(stack), PotionBundles.LINGERING_POTION_BUNDLE.get());
     }
 }
